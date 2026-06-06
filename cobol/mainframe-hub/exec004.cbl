98605f1c1671f44aebe56d5d2dba697903ed74dd
Eliel Miranda <ELIEL.MIRANDA.SILVA@GMAIL.COM>
2026-06-05 21:50:13 -0300

    Create exec004.cbl

---
+++ cobol/mainframe-hub/exec004.cbl
@@ -0,0 +1,22 @@
+      *================================================================*
+      *Este programa recebe um numero inteiro e infoma se ele eh par ou*
+      *impar                                                           *
+      *================================================================*
+ 
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. EXEC004.
+       AUTHOR. ELIEL MIRANDA.
+
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION. 
+          01 NUMERO.
+             05 WS-NUMERO PIC 9(05) VALUE ZEROES.
+
+           
+
+       PROCEDURE DIVISION.
+
+
+
+      
