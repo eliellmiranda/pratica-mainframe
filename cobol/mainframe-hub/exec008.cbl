9cb6624c3544137b0e02fefaef95fb2219453dcf
Eliel Miranda <ELIEL.MIRANDA.SILVA@GMAIL.COM>
2026-06-11 22:55:05 -0300

    Create exec008.cbl

---
+++ cobol/mainframe-hub/exec008.cbl
@@ -0,0 +1,19 @@
+      *================================================================*
+      *Este programa calcula o fatorial de um numero                   *
+      *================================================================*
+      
+       IDENTIFICATION DIVISION. 
+       PROGRAM-ID. EXEC008.
+       AUTHOR. ELIEL MIRANDA.
+
+       ENVIRONMENT DIVISION. 
+
+       DATA DIVISION. 
+
+       PROCEDURE DIVISION.
+
+       
+
+
+     
+
